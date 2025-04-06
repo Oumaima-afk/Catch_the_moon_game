8502b9793bbdf9c43b327f504b925aeb6a0ef404
Oumaima-afk <oumaima_afakkir@hotmail.com>
2025-04-06 22:25:01 +0200

    add character selection and falling objects

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
 // ajouter un addevent listener qq part
 const player = document.querySelector(".player");
 const gameArea = document.querySelector(".game-area");
+const scoreEl = document.querySelector(".score");
+const lifeNb = document.querySelector(".life");
 
 // Calculer la position centrale
 const middle = (gameArea.offsetWidth - player.offsetWidth) / 2;
@@ -32,3 +34,95 @@ function handleKeypress(event) {
   }
   event.preventDefault();
 }
+
+// ETAPE 2 : les objets qui tombent
+
+let score = 0;
+
+// nombre de chances au départ
+let life = 3;
+
+// objet non attrapé
+let missedCount = 0;
+
+// Fin de jeu
+let gameIsOver = false;
+
+// Le nb de vies disponibles
+function updateLifeDisplay() {
+  lifeNb.textContent = `Life : ${"❤️".repeat(life)}`;
+}
+updateLifeDisplay();
+
+// Handle the creation of falling objects
+function createFallingObjects() {
+  // Si le jeu est finit les objets arretent de tomber
+  if (gameIsOver) return;
+
+  let objectPosition = 0;
+
+  // Créer un objet
+  const object = document.createElement("div");
+  const img = document.createElement("img");
+  img.src = "../assets/star.png";
+  img.alt = "Falling object";
+  object.appendChild(img); // Ajouter l'image dans le div
+
+  object.classList.add("falling-object");
+
+  // Ajouter l'objet dans .game-area
+  gameArea.append(object);
+
+  object.style.left = Math.random() * maxLimite + "px";
+
+  // Lui donner une position aléatoire en haut de .game-area
+  const objectInterval = setInterval(() => {
+    // On arrête l'interval quand le jeu est finit
+    if (gameIsOver) {
+      clearInterval(objectInterval);
+      object.remove();
+      return;
+    }
+
+    objectPosition += 5;
+    object.style.top = objectPosition + "px";
+
+    // Détécter s'il touche le player
+    const playerRect = player.getBoundingClientRect();
+    const objectRect = object.getBoundingClientRect();
+
+    if (
+      objectRect.bottom >= playerRect.top &&
+      objectRect.left <= playerRect.right &&
+      objectRect.right >= playerRect.left
+    ) {
+      // on augmente le score
+      score++;
+      scoreEl.textContent = `Score : ${score}`;
+      object.remove();
+      clearInterval(objectInterval);
+    }
+
+    // Vérifier si l'objet n'est pas attrapé
+    if (objectPosition > gameArea.offsetHeight) {
+      missedCount++;
+      life--; // on enlève une vie
+      updateLifeDisplay(); // on met à jour l'affichage
+      object.remove();
+      clearInterval(objectInterval);
+    }
+
+    // Game over
+    if (life <= 0 && !gameIsOver) {
+      gameOver();
+    }
+  }, 50);
+}
+
+function gameOver() {
+  gameIsOver = true;
+  alert(`Game over... Votre score est : ${score}`);
+  window.location.reload();
+}
+
+setInterval(createFallingObjects, 2500);
